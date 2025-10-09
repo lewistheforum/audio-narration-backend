940249f816c16b1c9e616c21d72193682fd99ac8
lewisnguyenn <hieu.builder@gmail.com>
2025-10-09 15:39:07 +0700

    setup: socket io

--- src/modules/socket-gateway/gateway.ts
+++ src/modules/socket-gateway/gateway.ts
@@ -1,13 +1,45 @@
+import { OnModuleInit } from '@nestjs/common';
 import {
   MessageBody,
   SubscribeMessage,
   WebSocketGateway,
+  WebSocketServer,
 } from '@nestjs/websockets';
+import { Server } from 'socket.io';
 
-@WebSocketGateway()
-export class SocketGateway {
-  @SubscribeMessage('newMessage')
-  onNewMessage(@MessageBody() body: any) {
+@WebSocketGateway({
+  cors: {
+    origin: '*',
+    credentials: true,
+  },
+  namespace: 'socket-gateway',
+})
+export class SocketGateway implements OnModuleInit {
+  @WebSocketServer()
+  server: Server;
+
+  onModuleInit() {
+    this.server.on('connection', (socket) => {
+      console.log(socket.id);
+      console.log('Socket Connected');
+    });
+  }
+
+  @SubscribeMessage('updateSchedule')
+  onUpdateSchedule(@MessageBody() body: any) {
     console.log(body);
+    this.server.emit('updateScheduleData', {
+      message: 'Schedule updated',
+      data: body,
+    });
+  }
+
+  @SubscribeMessage('newMessageChat')
+  onNewMessageChat(@MessageBody() body: any) {
+    console.log(body);
+    this.server.emit(`onNewMessageChat-${body.conversationid}`, {
+      message: 'New message received',
+      data: body,
+    });
   }
 }
--- src/modules/socket-gateway/socket-gateway.module.ts
+++ src/modules/socket-gateway/socket-gateway.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SocketGatewayService } from './socket-gateway.service';
 import { SocketGatewayController } from './socket-gateway.controller';
+import { SocketGateway } from './gateway';
 
 @Module({
   controllers: [SocketGatewayController],
-  providers: [SocketGatewayService],
+  providers: [SocketGatewayService, SocketGateway],
 })
 export class SocketGatewayModule {}
