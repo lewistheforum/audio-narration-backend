e6d7e78006303f4b4e9bf01d5e9b000321de7139
ThanhMP <phucthanh2004ct@gmail.com>
2026-01-04 18:14:13 +0700

    fix: duplicate entity code_verification

--- src/modules/mailer/mailer.module.ts
+++ src/modules/mailer/mailer.module.ts
@@ -2,16 +2,13 @@ import { Module, forwardRef } from '@nestjs/common';
 import { MailerService } from './mailer.service';
 import { MailerController } from './mailer.controller';
 import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { PassportModule } from '@nestjs/passport';
 import { AccountsModule } from '../accounts/accounts.module';
-import { CodeVerification } from './entities/mailer.entity';
 
 @Module({
   imports: [
     PassportModule.register({ defaultStrategy: 'jwt' }),
     ConfigModule,
-    TypeOrmModule.forFeature([CodeVerification]),
     forwardRef(() => AccountsModule), // Circular dependency with AccountsModule
   ],
   controllers: [MailerController],
