21ece30acdd0b1dd331c4c992dc3b95213da1d65
lewisnguyenn <hieu.builder@gmail.com>
2026-06-22 22:13:25 +0700

    update: fix bug

--- src/modules/audio-narration/audio-narration.module.ts
+++ src/modules/audio-narration/audio-narration.module.ts
@@ -1,19 +1,29 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { Video, VideoSchema } from '../../database/schemas/video.schema';
-import { ProcessingJob, ProcessingJobSchema } from '../../database/schemas/processing-job.schema';
-import { VideoMetadata, VideoMetadataSchema } from '../../database/schemas/video-metadata.schema';
-import { Transcript, TranscriptSchema } from '../../database/schemas/transcript.schema';
+import { ProcessingTracking, ProcessingTrackingSchema } from '../../database/schemas/processing-tracking.schema';
+import { RawSource, RawSourceSchema } from '../../database/schemas/raw-source.schema';
+import { AudioSplitHandler, AudioSplitHandlerSchema } from '../../database/schemas/audio-split-handler.schema';
+import { AudioBackgroundHandler, AudioBackgroundHandlerSchema } from '../../database/schemas/audio-background-handler.schema';
+import { SttHandler, SttHandlerSchema } from '../../database/schemas/stt-handler.schema';
+import { TranslateScriptHandler, TranslateScriptHandlerSchema } from '../../database/schemas/translate-script-handler.schema';
+import { TtsHandler, TtsHandlerSchema } from '../../database/schemas/tts-handler.schema';
+import { MergeVoiceHandler, MergeVoiceHandlerSchema } from '../../database/schemas/merge-voice-handler.schema';
+import { FinalSource, FinalSourceSchema } from '../../database/schemas/final-source.schema';
 import { AudioNarrationService } from './audio-narration.service';
 import { AudioNarrationController } from './audio-narration.controller';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
-      { name: Video.name, schema: VideoSchema },
-      { name: ProcessingJob.name, schema: ProcessingJobSchema },
-      { name: VideoMetadata.name, schema: VideoMetadataSchema },
-      { name: Transcript.name, schema: TranscriptSchema },
+      { name: ProcessingTracking.name, schema: ProcessingTrackingSchema },
+      { name: RawSource.name, schema: RawSourceSchema },
+      { name: AudioSplitHandler.name, schema: AudioSplitHandlerSchema },
+      { name: AudioBackgroundHandler.name, schema: AudioBackgroundHandlerSchema },
+      { name: SttHandler.name, schema: SttHandlerSchema },
+      { name: TranslateScriptHandler.name, schema: TranslateScriptHandlerSchema },
+      { name: TtsHandler.name, schema: TtsHandlerSchema },
+      { name: MergeVoiceHandler.name, schema: MergeVoiceHandlerSchema },
+      { name: FinalSource.name, schema: FinalSourceSchema },
     ]),
   ],
   controllers: [AudioNarrationController],
